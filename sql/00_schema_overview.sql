25b6ff2e083b4a74706360ed6b28051e83ae7baf
Antoine Moineaux <antoine.moineaux@gmail.com>
2026-01-19 12:00:11 +0100

    Update 00_schema_overview.sql

--- sql/00_schema_overview.sql
+++ sql/00_schema_overview.sql
@@ -1,1 +1,26 @@
+-- Database schema overview
+-- Source: SQLite database (fittrack)
+-- Purpose: document core tables used for analytics
 
+-- USERS
+-- user_id (PK)
+-- signup_date
+-- country
+
+-- WORKOUTS
+-- workout_id (PK)
+-- user_id (FK -> users.user_id)
+-- workout_date
+-- workout_type
+
+-- EVENTS
+-- event_id
+-- user_id
+-- event_type
+-- event_date
+
+-- SUBSCRIPTIONS
+-- user_id
+-- start_date
+-- end_date
+-- plan_type
